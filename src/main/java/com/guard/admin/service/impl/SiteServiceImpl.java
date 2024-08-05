ab639464f3e59e971bcb992d36ad147b03dfe759
feenix <feenixnet@gmail.com>
2024-08-05 06:11:21 -0600

    site remove bug fix

--- src/main/java/com/guard/admin/service/impl/SiteServiceImpl.java
+++ src/main/java/com/guard/admin/service/impl/SiteServiceImpl.java
@@ -142,19 +142,23 @@ public class SiteServiceImpl implements SiteService {
     @Transactional
     public void delete(Integer id) {
         System.out.println(id);
-        siteRepository.deleteById(id);
         hitPointRepository.deleteAllBySiteId(id);
         shiftRepository.deleteAllBySiteId(id);
-        reportRepository.deleteAllBySiteId(id);
         scheduleRepository.deleteAllBySiteId(id);
         photoRepository.deleteAllBySiteId(id);
         visitorRepository.deleteAllBySiteId(id);
+        
         List<Report> results = reportRepository.findAllBySiteId(id);
         for(Report report : results) {
             Integer targetReportId = report.getId();
             reportPhotoRepository.deleteByReportId(targetReportId);
         }
+        
+        reportRepository.deleteAllBySiteId(id);
+        
+        siteRepository.deleteById(id);
     }
+    
 
     @Override
     public List<SiteWithHitpoint> getByUser(Integer id) {
